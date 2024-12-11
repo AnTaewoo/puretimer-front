aa2dab39e716b438ede56e6b36e15aa74dcc499b
AnTaewoo <atw13730@gmail.com>
2024-12-11 14:50:19 +0900

    FEAT: not yet error

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ dotenv.config();
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: "http://127.0.0.1:8080/", //process.env.VITE_API_URL
+      '/': {
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // path
+        rewrite: (path) => path, // path
       },
     },
   },
